564587c3cec03f2e9894e378b292e53825ff31cb
Pectics <pectics@outlook.com>
2022-10-04 23:43:09 +0800

    rewrite bing pic

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    res.json({
+    res.status(400).jsonp({
         code: 400,
         message: 'Error! Please call the correct API.',
         advice: 'Refer to docs.peckot.com for more.'
