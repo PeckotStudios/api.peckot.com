5621a4beb0ab99edcf678684582fdba0a1e5364d
Pectics <pectics@outlook.com>
2022-11-19 19:40:21 +0800

    PCLHP

--- api/PCL2HomePage.js
+++ api/PCL2HomePage.js
@@ -41,6 +41,7 @@ module.exports = (req, res) => {
             return;
         });    
     }
+    source = source.replace(/\$\(broadcast\)/, '当前没有公告');
     MinecraftServerListPing.ping(4, config.server.host, config.server.port, 3000)
     .then(response => {
         source = source.replace(/\$\(status\)/, '在线');
@@ -53,15 +54,15 @@ module.exports = (req, res) => {
             });
             return playerlist;
         }));
+        res.status(200).send(source);
     })
     .catch(error => {
         source = source.replace(/\$\(status\)/, '离线');
         source = source.replace(/\$\(online\)/, 'NaN');
         source = source.replace(/\$\(max\)/, 'NaN');
         source = source.replace(/\$\(playerlist\)/, '无数据');
+        res.status(200).send(source);
     });
-    source = source.replace(/\$\(broadcast\)/, '当前没有公告');
 
-    res.status(200).send(source);
 
 }
