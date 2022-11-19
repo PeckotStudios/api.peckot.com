03ad4c1c9afb28572969cf28becdafaf5af3f531
Pectics <pectics@outlook.com>
2022-11-19 20:47:50 +0800

    1

--- api/PCL2HomePage.js
+++ api/PCL2HomePage.js
@@ -41,7 +41,7 @@ module.exports = (req, res) => {
     //         return;
     //     });    
     // }
-    source = source.replace(/\$\(broadcast\)/, '当前没有公告' + config);
+    source = source.replace(/\$\(broadcast\)/, '当前没有公告' + config + config.server.host);
     MinecraftServerListPing.ping(4, config.server.host, config.server.port, 3000)
     .then(response => {
         source = source.replace(/\$\(status\)/, '在线');
