1a8c146759ac5b61df282260d7ecff74c21c29e4
Pectics <pectics@outlook.com>
2022-11-20 17:36:33 +0800

    hitokoto fix

--- api/PCL2HomePage.js
+++ api/PCL2HomePage.js
@@ -62,18 +62,18 @@ module.exports = (req, res) => {
                 });
         },
         function(callback) {
-            axios.get('https://v1.hitokoto.cn')
-                .then(({ response }) => callback(null, response.hitokoto))
+            axios.get('https://v1.hitokoto.cn/?encode=text')
+                .then(response => callback(null, response))
                 .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
         },
         function(callback) {
-            axios.get('https://v1.hitokoto.cn')
-                .then(({ response }) => callback(null, response.hitokoto))
+            axios.get('https://v1.hitokoto.cn/?encode=text')
+                .then(response => callback(null, response))
                 .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
         },
         function(callback) {
-            axios.get('https://v1.hitokoto.cn')
-                .then(({ response }) => callback(null, response.hitokoto))
+            axios.get('https://v1.hitokoto.cn/?encode=text')
+                .then(response => callback(null, response))
                 .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
         }
     ],
