d3c6a17644649f2f95bc4202df23752e2b15cf00
Pectics <pectics@outlook.com>
2022-11-21 00:34:34 +0800

    fixed limit & delete files

--- api/PCL2HomePage.js
+++ api/PCL2HomePage.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
     // Dependencies
     const api = require('../.lib/API')(res);
-    const { MinecraftServerListPing } = require("minecraft-status");
+    const mslp = require("minecraft-status").MinecraftServerListPing;
     const async = require('async');
     const axios = require('axios');
     const cookie = require('cookie');
@@ -24,75 +24,68 @@ module.exports = (req, res) => {
         return;
     }
 
+    // Functions
+    function hitokoto(callback) {
+        axios.get('https://v1.hitokoto.cn')
+            .then(({ data }) => callback(null, data.hitokoto))
+            .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
+    }
+
     // Placeholders replacement
     source = source.replace(/\$\(broadcast\)/, '当前没有公告');
 
     // Request limit
     if (cookies.data != undefined) data = JSON.parse(cookies.data);
-    if (time.getTime() - data.ti <= parseInt(interval)) {
-        let ti = new Date(data.ti);
-        if (data.st == '在线') {
-            let online = parseInt(data.ol.split('/')[0]);
-            if (online <= 0) {
-                source = source.replace(/\$\(textlist\)/, '无玩家');
-            } else if (online >= 2) {
-                source = source
-                    .replace(/\$\(textlist\)/, '')
-                    .replace(/\<\!\-\-\$\(showlist\)|\$\(showlist\)\-\-\>/g, '')
-                    .replace(/\$\(playerlist\)/, data.pl);
-            } else {
-                source = source.replace(/\$\(textlist\)/, data.pl);
-            }
-        } else {
-            source = source.replace(/\$\(textlist\)/, '无数据');
-        }
-        source = source
-            .replace(/\$\(status\)/, data.st)
-        	.replace(/\$\(online\)/, data.ol)
-        	.replace(/\$\(hitokoto1\)/, data.h1)
-        	.replace(/\$\(hitokoto2\)/, data.h2)
-        	.replace(/\$\(hitokoto3\)/, data.h3)
-        	.replace(/\$\(time\)/,`${ti.getMonth()+1}/${ti.getDate()} ${ti.getHours()}:${ti.getMinutes()}:${ti.getSeconds()}`);
-        res.status(200).setHeader('Content-Type', 'application/json').send(source);
-    } else {
+    // if (time.getTime() - data.ti <= parseInt(interval)) {
+    //     let ti = new Date(data.ti);
+    //     if (data.st == '在线') {
+    //         let online = parseInt(data.ol.split('/')[0]);
+    //         if (online <= 0) {
+    //             source = source.replace(/\$\(textlist\)/, '无玩家');
+    //         } else if (online >= 2) {
+    //             source = source
+    //                 .replace(/\$\(textlist\)/, '')
+    //                 .replace(/\<\!\-\-\$\(showlist\)|\$\(showlist\)\-\-\>/g, '')
+    //                 .replace(/\$\(playerlist\)/, data.pl);
+    //         } else {
+    //             source = source.replace(/\$\(textlist\)/, data.pl);
+    //         }
+    //     } else {
+    //         source = source.replace(/\$\(textlist\)/, '无数据');
+    //     }
+    //     source = source
+    //         .replace(/\$\(status\)/, data.st)
+    //     	.replace(/\$\(online\)/, data.ol)
+    //     	.replace(/\$\(hitokoto1\)/, data.h1)
+    //     	.replace(/\$\(hitokoto2\)/, data.h2)
+    //     	.replace(/\$\(hitokoto3\)/, data.h3)
+    //     	.replace(/\$\(time\)/,`${ti.getMonth()+1}/${ti.getDate()} ${ti.getHours()}:${ti.getMinutes()}:${ti.getSeconds()}`);
+    //     res.status(200).setHeader('Content-Type', 'application/json').send(source);
+    // } else {
         async.series([
-            function(callback) {MinecraftServerListPing.ping(4, host, port, 10000)
-                .then(response => {
-                    result = {
-                        status: '在线',
-                        online: `${response.players.online}/${response.players.max}`,
-                        playerlist: (() => {
-                            let playerlist = [];
-                            response.players.sample.forEach(player => {
-                                playerlist.push(`${player.name}`);
-                            });
-                            return playerlist.join('，');
-                        })()
-                    };
-                    callback(null, result);
-                })
-                .catch(ignore => {
-                    callback(null, {
-                        status: '离线',
-                        online: '无数据'
+            function(callback) {
+                mslp.ping(4, host, port, 10000)
+                    .then(response => {
+                        result = {
+                            status: '在线',
+                            online: `${response.players.online}/${response.players.max}`,
+                            playerlist: (() => {
+                                let playerlist = [];
+                                response.players.sample.forEach(player => {
+                                    playerlist.push(`${player.name}`);
+                                });
+                                return playerlist.join('，');
+                            })()
+                        };
+                        callback(null, result);
+                    })
+                    .catch(ignore => {
+                        callback(null, {
+                            status: '离线',
+                            online: '无数据'
+                        });
                     });
-                });
-            },
-            function(callback) {
-                axios.get('https://v1.hitokoto.cn')
-                    .then(({ data }) => callback(null, data.hitokoto))
-                    .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
-            },
-            function(callback) {
-                axios.get('https://v1.hitokoto.cn')
-                    .then(({ data }) => callback(null, data.hitokoto))
-                    .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
-            },
-            function(callback) {
-                axios.get('https://v1.hitokoto.cn')
-                    .then(({ data }) => callback(null, data.hitokoto))
-                    .catch(ignore => callback(null, 'Error: 一言语录获取失败'));
-            }
+            }, hitokoto, hitokoto, hitokoto
         ],
             function(ignore, result) {
                 if (result[0].status == '在线') {
@@ -116,19 +109,19 @@ module.exports = (req, res) => {
                 	.replace(/\$\(hitokoto2\)/, result[2])
                 	.replace(/\$\(hitokoto3\)/, result[3])
                 	.replace(/\$\(time\)/,`${time.getMonth()+1}/${time.getDate()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
-                res.setHeader('Set-Cookie', cookie.serialize('data', JSON.stringify({
-                    ti: time.getTime(),
-                    st: result[0].status,
-                    ol: result[0].online,
-                    pl: result[0].playerlist,
-                    h1: result[1],
-                    h2: result[2],
-                    h3: result[3]
-                }), {httpOnly: false}));
+                // res.setHeader('Set-Cookie', cookie.serialize('data', JSON.stringify({
+                //     ti: time.getTime(),
+                //     st: result[0].status,
+                //     ol: result[0].online,
+                //     pl: result[0].playerlist,
+                //     h1: result[1],
+                //     h2: result[2],
+                //     h3: result[3]
+                // }), {httpOnly: true}));
                 res.status(200).setHeader('Content-Type', 'application/json').send(source);
             }
         );
-    }
+    // }
     
 
 }
