4c5090ae9c70aa2c107b14da8f759f2ac2e351e7
Pectics <pectics@outlook.com>
2023-03-18 17:16:34 +0800

    ddg

--- api/DuckDuckGoSearch.js
+++ api/DuckDuckGoSearch.js
@@ -1,58 +1,58 @@
 import { info as $info, error as $error } from "../.lib/API";
-import { URLSearchParams } from "url";
 import { JSDOM } from "jsdom";
-import axios from 'axios';
+import axios from "axios";
 
 export default (req, res) => {
   // Input arguments
   const {
-    key = 'Peckot API',
-    num = 5,
+    keyword = 'Peckot API',
+    amount = 5,
+    time = null,
   } = req.query;
 
   // Main process
   res.setHeader("Content-Type", "application/json")
-  getResults(key)
+  getResults(keyword, amount, time)
     .then(data => res.status(200).send($info(data)))
     .catch(error => res.status(400).send($error(error)));
-
 }
 
 // 获取HTML页面内容
-async function getHtml(keyword) {
-  const url = 'http://html.duckduckgo.com/html/';
-  const data = new URLSearchParams();
-  data.append('q', keyword);
-  const { data: html } = await axios({
-    method: 'POST',
-    url: url,
-    data: data,
-  });
-  return html;
+async function getHtml(keyword, time) {
+  switch (time) {
+    case "year": time = "y"; break;
+    case "month": time = "m"; break;
+    case "week": time = "w"; break;
+    case "d": time = "d"; break;
+    default: time = ""; break;
+  }
+  const url = `http://html.duckduckgo.com/html/?q=${keyword}&df=${time}`;
+  const { data } = await axios.get(url);
+  return data;
 }
 
 // 解析HTML页面内容，返回搜索结果的数组
 function parseHtml(html) {
-  const final_results = [];
+  const finalResults = [];
   const dom = new JSDOM(html);
-  const result_bodies = dom.window.document.querySelectorAll(".result__body");
-  result_bodies.forEach(item => {
+  const resultBodies = dom.window.document.querySelectorAll(".result__body");
+  resultBodies.forEach(item => {
     const title = item.querySelector(".result__a").textContent;
-    //const link = decodeURIComponent(item.querySelector(".result__a").href.substring("//duckduckgo.com/l/?uddg=".length));
-    const link = item.querySelector(".result__a").href;
+    const link = decodeURIComponent(item.querySelector(".result__a").href.substring("//duckduckgo.com/l/?uddg=".length));
     const snippet = item.querySelector(".result__snippet").textContent;
-    final_results.push({
+    finalResults.push({
       title: title,
       link: link,
       snippet: snippet
     });
   });
-  return Promise.resolve(final_results);
+  return Promise.resolve(finalResults);
 }
 
 // 获取搜索结果并解析
-async function getResults(keyword) {
-  const html = await getHtml(keyword);
+async function getResults(keyword, amount, time) {
+  const html = await getHtml(keyword, time);
   const resultsArray = await parseHtml(html);
-  return resultsArray;
+  const mergedArray = [].concat(...resultsArray.slice(0, amount));
+  return Promise.resolve(mergedArray);
 }
