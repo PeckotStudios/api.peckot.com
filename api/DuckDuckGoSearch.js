7028e63bebb1ee945080c8f8cd82a6fed9d9bf13
Pectics <pectics@outlook.com>
2023-03-18 20:24:07 +0800

    ddg

--- api/DuckDuckGoSearch.js
+++ api/DuckDuckGoSearch.js
@@ -10,7 +10,7 @@ export default (req, res) => {
     time = null,
   } = req.query;
 
-  const _keyword = decodeURIComponent(keyword);
+  const _keyword = encodeURIComponent(keyword);
 
   // Main process
   res.setHeader("Content-Type", "application/json")
